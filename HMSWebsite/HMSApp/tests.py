ebd1a63505f299a3d5795ba7c776c722537e4ec2
Suhas <suhasbs.nitk@gmail.com>
2018-03-22 00:03:58 +0530

    Static images

--- HMSWebsite/HMSApp/tests.py
+++ HMSWebsite/HMSApp/tests.py
@@ -20,108 +20,166 @@ class LoginTestCase(LiveServerTestCase):
         self.selenium.quit()
         super(LoginTestCase, self).tearDown()
 
-    def test_register(self):
+    def test_case1(self):
         selenium = self.selenium
         #Opening the link we want to test
         selenium.get('http://127.0.0.1:8000/login/')
         #find the form element
         username = selenium.find_element_by_name('username')
         password = selenium.find_element_by_name('password')
-        print username, password
+        print "Testing for correct username and wrong password"
         submit = selenium.find_element_by_tag_name('button')
-        print submit
+        
         username.send_keys('suhas')
         password.send_keys('suhas1234')
         submit.click()
         assert 'Wrong Login Credentials!' in selenium.page_source
-
+        print "Test Case Passed\n\n"
         # username.send_keys('suhas')
         # password.send_keys('suhas123')
         # submit.send_keys(Keys.RETURN)
         # assert 'Wrong Login Credentials!' in selenium.page_source
 
-class LoginTestCase2(LiveServerTestCase):
-
-    def setUp(self):
-        self.selenium = webdriver.Firefox()
-        self.selenium.implicitly_wait(2)
-        super(LoginTestCase2, self).setUp()
-
-    def tearDown(self):
-        self.selenium.quit()
-        super(LoginTestCase2, self).tearDown()
-
-    def test_register(self):
+    def test_case2(self):
         selenium = self.selenium
-        #Opening the link we want to test
+        #Opening the link we want to tests
         selenium.get('http://127.0.0.1:8000/login/')
         #find the form element
         username = selenium.find_element_by_name('username')
         password = selenium.find_element_by_name('password')
-        print username, password
+        print "Testing for wrong username and wrong password"
         submit = selenium.find_element_by_tag_name('button')
-        print submit
+      
         username.send_keys('')
         password.send_keys('suhas1234')
         submit.click()
         # print selenium.page_source
-        time.sleep(1)
+        
         assert 'Wrong Login Credentials!' in selenium.page_source
+        print "Test Case Passed\n\n"
 
-class LoginTestCase3(LiveServerTestCase):
-
-    def setUp(self):
-        self.selenium = webdriver.Firefox()
-        self.selenium.implicitly_wait(2)
-        super(LoginTestCase3, self).setUp()
-
-    def tearDown(self):
-        self.selenium.quit()
-        super(LoginTestCase3, self).tearDown()
-
-    def test_register(self):
+    def test_case3(self):
         selenium = self.selenium
         #Opening the link we want to test
         selenium.get('http://127.0.0.1:8000/login/')
         #find the form element
         username = selenium.find_element_by_name('username')
         password = selenium.find_element_by_name('password')
-        print username, password
+        print "Testing for wrong username and correct password"
         submit = selenium.find_element_by_tag_name('button')
-        print submit
+        
         username.send_keys('')
         password.send_keys('suhas123')
         submit.click()
         # print selenium.page_source
-        time.sleep(1)
+        
         assert 'Wrong Login Credentials!' in selenium.page_source
+        print "Test Case Passed\n\n"
 
-
-class LoginTestCase4(LiveServerTestCase):
-
-    def setUp(self):
-        self.selenium = webdriver.Firefox()
-        self.selenium.implicitly_wait(2)
-        super(LoginTestCase4, self).setUp()
-
-    def tearDown(self):
-        # self.selenium.quit()
-        super(LoginTestCase4, self).tearDown()
-
-    def test_register(self):
+    def test_case4(self):
         selenium = self.selenium
         #Opening the link we want to test
         selenium.get('http://127.0.0.1:8000/login/')
         #find the form element
         username = selenium.find_element_by_name('username')
         password = selenium.find_element_by_name('password')
-        print username, password
+        print "Testing for correct username and correct password"
         submit = selenium.find_element_by_tag_name('button')
-        print submit
+        
         username.send_keys('suhas')
-        password.send_keys('suhas123')
+        password.send_keys('suhas123')																										
         submit.click()
         # print selenium.page_source
         # time.sleep(10)
         # print selenium.page_source
         assert 'Dashboard' in selenium.page_source
+        print "Test Case Passed\n\n"
+
+# class LoginTestCase2(LiveServerTestCase):
+
+#     def setUp(self):
+#         self.selenium = webdriver.Firefox()
+#         self.selenium.implicitly_wait(2)
+#         super(LoginTestCase2, self).setUp()
+
+#     def tearDown(self):
+#         self.selenium.quit()
+#         super(LoginTestCase2, self).tearDown()
+
+#     def test_case2(self):
+#         selenium = self.selenium
+#         #Opening the link we want to tests
+#         selenium.get('http://127.0.0.1:8000/login/')
+#         #find the form element
+#         username = selenium.find_element_by_name('username')
+#         password = selenium.find_element_by_name('password')
+#         print "Testing for wrong username and wrong password"
+#         submit = selenium.find_element_by_tag_name('button')
+      
+#         username.send_keys('')
+#         password.send_keys('suhas1234')
+#         submit.click()
+#         # print selenium.page_source
+        
+#         assert 'Wrong Login Credentials!' in selenium.page_source
+#         print "Test Case Passed\n\n"
+
+# class LoginTestCase3(LiveServerTestCase):
+
+#     def setUp(self):
+#         self.selenium = webdriver.Firefox()
+#         self.selenium.implicitly_wait(2)
+#         super(LoginTestCase3, self).setUp()
+
+#     def tearDown(self):
+#         self.selenium.quit()
+#         super(LoginTestCase3, self).tearDown()
+
+#     def test_case3(self):
+#         selenium = self.selenium
+#         #Opening the link we want to test
+#         selenium.get('http://127.0.0.1:8000/login/')
+#         #find the form element
+#         username = selenium.find_element_by_name('username')
+#         password = selenium.find_element_by_name('password')
+#         print "Testing for wrong username and correct password"
+#         submit = selenium.find_element_by_tag_name('button')
+        
+#         username.send_keys('')
+#         password.send_keys('suhas123')
+#         submit.click()
+#         # print selenium.page_source
+        
+#         assert 'Wrong Login Credentials!' in selenium.page_source
+#         print "Test Case Passed\n\n"
+
+
+# class LoginTestCase4(LiveServerTestCase):
+
+#     def setUp(self):
+#         self.selenium = webdriver.Firefox()
+#         self.selenium.implicitly_wait(2)
+#         super(LoginTestCase4, self).setUp()
+
+#     def tearDown(self):
+#         # self.selenium.quit()
+#         super(LoginTestCase4, self).tearDown()
+
+#     def test_case4(self):
+#         selenium = self.selenium
+#         #Opening the link we want to test
+#         selenium.get('http://127.0.0.1:8000/login/')
+#         #find the form element
+#         username = selenium.find_element_by_name('username')
+#         password = selenium.find_element_by_name('password')
+#         print "Testing for correct username and correct password"
+#         submit = selenium.find_element_by_tag_name('button')
+        
+#         username.send_keys('suhas')
+#         password.send_keys('suhas123')																										
+#         submit.click()
+#         # print selenium.page_source
+#         # time.sleep(10)
+#         # print selenium.page_source
+#         assert 'Dashboard' in selenium.page_source
+#         print "Test Case Passed\n\n"
